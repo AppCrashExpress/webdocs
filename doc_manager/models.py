5bad6ae5fbcd88cd434911d0636791fe5a1ffcc9
AppCrashExpress <efimov.s2000@gmail.com>
2021-07-18 10:50:17 +0300

    Merge execution with order and add soft deletion

--- doc_manager/models.py
+++ doc_manager/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from softdelete.models import SoftDeleteObject
 
 class Address(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,37 +18,6 @@ class Material(models.Model):
 
     def __str__(self):
         return self.name
-
-class Specification(models.Model):
-    UNITS = (
-        ('m3', 'Кубические метры'),
-        ('t',  'Тонны'),
-    )
-
-    class Meta:
-        ordering = ['doc_no']
-
-    doc_no    = models.PositiveIntegerField(primary_key=True)
-    date      = models.DateField()
-    from_addr = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='spec_from_addr')
-    to_addr   = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='spec_to_addr')
-    material  = models.ForeignKey('Material', on_delete=models.PROTECT)
-    units     = models.CharField(max_length=3, choices=UNITS)
-    price     = models.PositiveIntegerField()
-
-class Order(models.Model):
-    date          = models.DateField()
-    specification = models.ForeignKey('Specification', on_delete=models.PROTECT)
-    customer      = models.ForeignKey('Customer',      on_delete=models.PROTECT)
-    count         = models.PositiveIntegerField()
-
-    @property
-    def price(self):
-        return self.specification.price * self.count
-
-    class Meta:
-        ordering = ['id']
-        unique_together = (('customer', 'specification'),)
 
 class VehicleModel(models.Model):
     model = models.CharField(max_length=255, unique=True)
@@ -68,12 +38,38 @@ class Driver(models.Model):
     def __str__(self):
         return self.name
 
-class Execution(models.Model):
-    order     = models.ForeignKey('Order',    on_delete=models.PROTECT)
-    vehicle   = models.ForeignKey('Vehicle',  on_delete=models.PROTECT, null=True)
-    driver    = models.ForeignKey('Driver',   on_delete=models.PROTECT, null=True)
-    from_addr = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='ord_from_addr', null=True)
-    to_addr   = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='ord_to_addr', null=True)
+class Specification(SoftDeleteObject, models.Model):
+    UNITS = (
+        ('m3', 'Кубические метры'),
+        ('t',  'Тонны'),
+    )
 
     class Meta:
-        ordering = ['order']
+        ordering = ['doc_no']
+
+    doc_no    = models.PositiveIntegerField(primary_key=True)
+    date      = models.DateField()
+    from_addr = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='spec_from_addr')
+    to_addr   = models.ForeignKey('Address',  on_delete=models.PROTECT, related_name='spec_to_addr')
+    material  = models.ForeignKey('Material', on_delete=models.PROTECT)
+    units     = models.CharField(max_length=3, choices=UNITS)
+    price     = models.PositiveIntegerField()
+
+class Order(SoftDeleteObject, models.Model):
+    date          = models.DateField()
+    specification = models.ForeignKey('Specification', on_delete=models.PROTECT)
+    customer      = models.ForeignKey('Customer',      on_delete=models.PROTECT)
+    count         = models.PositiveIntegerField()
+
+    vehicle        = models.ForeignKey('Vehicle', on_delete=models.PROTECT, null=True)
+    driver         = models.ForeignKey('Driver',  on_delete=models.PROTECT, null=True)
+    real_from_addr = models.ForeignKey('Address', on_delete=models.PROTECT, related_name='real_from_addr', null=True)
+    real_to_addr   = models.ForeignKey('Address', on_delete=models.PROTECT, related_name='real_to_addr', null=True)
+
+    @property
+    def price(self):
+        return self.specification.price * self.count
+
+    class Meta:
+        ordering = ['id']
+        unique_together = (('customer', 'specification'),)
