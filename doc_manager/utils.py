949d9499fe5582d83a9367fcffdcdc1dfda54810
AppCrashExpress <efimov.s2000@gmail.com>
2021-08-04 15:07:08 +0300

    Add queryset to excel worksheet formatter

--- doc_manager/utils.py
+++ doc_manager/utils.py
@@ -1,6 +1,10 @@
+import datetime
+from itertools import chain
+
 from django.shortcuts import render, redirect
 from django.urls      import reverse
 from django.contrib   import messages
+from openpyxl import Workbook
 
 def create_generic(
     request, *, form_class,
@@ -70,3 +74,61 @@ def delete_generic(request, pk, *, model_class, list_url_name):
     messages.success(request, message)
 
     return redirect(list_url_name)
+
+def get_verbose_name(model, string):
+    # Stackoverflow 52410442
+    fields = string.split('__')
+
+    for field_name in fields[:-1]:
+        field = model._meta.get_field(field_name)
+
+        if field.many_to_one:
+            model = field.foreign_related_fields[0].model
+        elif field.many_to_many or field.one_to_one or field.one_to_many:
+            model = field.related_model
+        else:
+            raise ValueError(f'Field not found: {field_name}')
+
+    return model._meta.get_field(fields[-1]).verbose_name
+
+def get_report_workbook(queryset, *, fields, verbose_names, display_values):
+    def normalize_value(value, display_values):
+        # Can modify if new special types arise
+        if value is None:
+            return ""
+
+        if isinstance(value, datetime.date):
+            return value.strftime('%d.%m.%Y')
+
+        dv = display_values.get(value, None)
+        if dv:
+            return dv
+
+        return str(value)
+
+    def get_field_names(fields, vebose_names, model):
+        field_names = []
+
+        for field in fields:
+            vb_name = verbose_names.get(field, None)
+            if vb_name is None:
+                vb_name = get_verbose_name(model, field)
+
+            field_names.append(vb_name)
+
+        return field_names
+
+    model = queryset.model
+    wb = Workbook()
+    ws = wb.active
+
+    field_names = get_field_names(fields, verbose_names, model)
+
+    ws.append(field_names)
+
+    normalizer_func = lambda x : normalize_value(x, display_values)
+    for value_set in queryset.values_list(*fields):
+        normalized_set = map(normalizer_func, value_set)
+        ws.append(tuple(normalized_set))
+
+    return wb
